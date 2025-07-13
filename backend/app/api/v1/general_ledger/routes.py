ef14e088ab01402df83be7aaeba909187765557d
Matthew Fay <mmfay3@gmail.com>
2025-07-13 13:18:02 -0700

    Add Main Accounts w/ Get/Post from backend

--- backend/app/api/v1/general_ledger/routes.py
+++ backend/app/api/v1/general_ledger/routes.py
@@ -1,8 +1,20 @@
-from fastapi import APIRouter
-from .service import get_trial_balance
+from fastapi import APIRouter, HTTPException
+from .service import *
+from .schemas import *
 
 router = APIRouter()
 
-@router.get("/")
-def trial_balance():
+@router.get("/trial_balance")
+def trial_balance() -> list[Account]:
     return get_trial_balance()
+
+@router.get("/main_accounts")
+def main_accounts() -> list[MainAccount]:
+    return get_main_accounts()
+
+@router.post("/main_accounts", response_model=MainAccount)
+def add_main_account(account: CreateMainAccount):
+    created = create_main_account(account)
+    if created is None:
+        raise HTTPException(status_code=400, detail="Account already exists.")
+    return created
